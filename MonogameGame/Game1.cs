673f382044cf67a9034967adcbfc3382dce33f2b
Ramonb2 <Ramon.brakels@student.stenden.com>
2023-03-22 00:49:03 +0100

    Map and entity objects update

--- MonogameGame/Game1.cs
+++ MonogameGame/Game1.cs
@@ -1,57 +1,108 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+using System.Threading;
 
-namespace MonogameGame
+namespace MonogameGame;
+public class Game1 : Game
 {
-    public class Game1 : Game
+    private GraphicsDeviceManager _graphics;
+    private SpriteBatch _spriteBatch;
+    private Texture2D _mapTexture;
+
+    private Player _player;
+    private List<Enemy> _enemies;
+
+    public Game1()
     {
-        private GraphicsDeviceManager graphics;
-        private SpriteBatch spriteBatch;
-
-        private int[,] map;
-        private Texture2D tileTexture;
-
-        public Game1()
-        {
-            graphics = new GraphicsDeviceManager(this);
-            Content.RootDirectory = "Content";
-        }
-
-        protected override void LoadContent()
-        {
-            spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            map = new MapGenerator(200, 200, 1).GenerateMap();
-
-            tileTexture = new Texture2D(GraphicsDevice, 1, 1);
-            tileTexture.SetData(new Color[] { Color.White });
-        }
-
-        protected override void Draw(GameTime gameTime)
-        {
-            GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            spriteBatch.Begin();
-
-            for (int x = 0; x < map.GetLength(0); x++)
-            {
-                for (int y = 0; y < map.GetLength(1); y++)
-                {
-                    if (map[x, y] == 0)
-                    {
-                        spriteBatch.Draw(Content.Load<Texture2D>("textureTest"), new Vector2(x * 64, y * 64), Color.White);
-                    }
-                    else
-                    {
-                        spriteBatch.Draw(Content.Load<Texture2D>("water"), new Vector2(x * 32, y * 32), Color.White);
-                    }
-                }
-            }
-
-            spriteBatch.End();
-
-            base.Draw(gameTime);
-        }
+        _graphics = new GraphicsDeviceManager(this);
+        Content.RootDirectory = "Content";
+        IsMouseVisible = true;
     }
+
+    protected override void Initialize()
+    {
+        base.Initialize();
+    }
+
+    private Texture2D GenerateMapTexture(int width, int height, float scale)
+    {
+        Texture2D texture = new Texture2D(GraphicsDevice, width, height);
+        Color[] colorData = new Color[width * height];
+
+        float[,] noiseMap = PerlinNoiseGenerator.GenerateNoiseMap(width, height, scale);
+
+        for (int y = 0; y < height; y++)
+        {
+            for (int x = 0; x < width; x++)
+            {
+                float value = noiseMap[x, y];
+                colorData[y * width + x] = new Color(value, value, value);
+            }
+        }
+
+        texture.SetData(colorData);
+        return texture;
+    }
+
+    protected override void LoadContent()
+    {
+        _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+        int mapWidth = 256;
+        int mapHeight = 256;
+        float scale = 25f;
+        _mapTexture = GenerateMapTexture(mapWidth, mapHeight, scale);
+
+        Texture2D playerTexture = Content.Load<Texture2D>("player");
+        _player = new Player(new Vector2(100, 100), 100, playerTexture);
+
+        Texture2D shipTexture = Content.Load<Texture2D>("player");
+        Texture2D monsterTexture = Content.Load<Texture2D>("player");
+        _enemies = new List<Enemy>
+        {
+            new Enemy(new Vector2(200, 200), 50, shipTexture),
+            new Enemy(new Vector2(300, 300), 30, monsterTexture)
+        };
+    }
+
+    protected override void Update(GameTime gameTime)
+    {
+        if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+            Exit();
+
+        _player.Update(gameTime);
+
+        foreach (Enemy enemy in _enemies)
+        {
+            enemy.Update(gameTime);
+            enemy.PerformAI(_player);
+        }
+
+        base.Update(gameTime);
+    }
+
+    protected override void Draw(GameTime gameTime)
+    {
+        GraphicsDevice.Clear(Color.CornflowerBlue);
+
+        _spriteBatch.Begin();
+
+        _spriteBatch.Draw(_mapTexture, Vector2.Zero, Color.White);
+
+        _player.Draw(_spriteBatch);
+
+        foreach (Enemy enemy in _enemies)
+        {
+            enemy.Draw(_spriteBatch);
+        }
+
+        _spriteBatch.End();
+
+        base.Draw(gameTime);
+    }
+
+    // Include the methods from the previous answer here
+    // GenerateMapTexture, GetColorFromValue, etc.
 }
